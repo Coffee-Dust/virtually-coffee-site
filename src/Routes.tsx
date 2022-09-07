48eae7505b072a88e48141af6f88e4c74efa2cb3
John Dust <d24dust@hotmail.com>
2022-09-07 16:37:25 -0400

    add: Route for /support to Support page cmpnt

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './pages/home';
+import Support from './pages/support';
 import Virt8bitPage from './pages/virtually8bit';
 
 function Routes() {
@@ -10,6 +11,9 @@ function Routes() {
       </Route>
       <Route path="/virtually8bit">
         <Virt8bitPage />
+      </Route>
+      <Route path="/support">
+        <Support />
       </Route>
       <Redirect from='*' to='/' />
     </Switch>
