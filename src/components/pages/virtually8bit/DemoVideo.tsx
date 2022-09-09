61e7c39df41e8f1c68322894959b55e5438f079c
John Dust <d24dust@hotmail.com>
2022-09-09 18:01:47 -0400

    add:render> iframe wrapper and iframe elements with dynamic src and styling

--- src/components/pages/virtually8bit/DemoVideo.tsx
+++ src/components/pages/virtually8bit/DemoVideo.tsx
@@ -13,7 +13,28 @@ const DemoVideo: React.FC<Props> = (props: Props) => {
 		<div id={props.id} style={{
 			maxWidth: `${props.widthPercentage || 100}%`
 		}}>
+			<div style={{
+				overflow: "hidden",
+				aspectRatio: "19.5/9",
+				pointerEvents: "none",
+				borderRadius: "40px",
+				transform: "translateZ(0px)",
+				boxShadow: "-2px 5px 10px #000000"
+			}}>
+				<iframe 
+				id="ytplayer"
+				title="Youtube video"
+				src={`https://www.youtube.com/embed/${props.videoID}?autoplay=1&origin=${origin}&controls=0&disablekb=1&fs=0&loop=1&mute=1&playlist=${props.videoID}`}
+				allowFullScreen={true}
+				allow="autoplay"
+				style={{
+					width: "300%",
+					height: "100%",
+					marginLeft: "-100%",
+					border: "none"
+				}}></iframe>
 			
+			</div>
 		</div>
 	)
 }
