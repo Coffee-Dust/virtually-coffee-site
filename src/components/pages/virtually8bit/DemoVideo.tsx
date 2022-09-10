e7f65d8a68387b45960ca74b656c4eeb066f893a
John Dust <d24dust@hotmail.com>
2022-09-10 13:15:23 -0400

    feat:render> will now show thumbnail on mobile browser instead of iframe

--- src/components/pages/virtually8bit/DemoVideo.tsx
+++ src/components/pages/virtually8bit/DemoVideo.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { BrowserView, MobileView } from 'react-device-detect';
 
 interface Props {
 	id: string,
 	videoID: string,
-	widthPercentage?: number
+	widthPercentage?: number,
+	mobileThumbnail: string
 }
 
 const DemoVideo: React.FC<Props> = (props: Props) => {
@@ -13,28 +15,33 @@ const DemoVideo: React.FC<Props> = (props: Props) => {
 		<div id={props.id} style={{
 			maxWidth: `${props.widthPercentage || 100}%`
 		}}>
-			<div style={{
-				overflow: "hidden",
-				aspectRatio: "19.5/9",
-				pointerEvents: (window.innerWidth >= 800 ? "none" : "all"),
-				borderRadius: "40px",
-				transform: "translateZ(0px)",
-				boxShadow: "-2px 5px 10px #000000"
-			}}>
-				<iframe 
-				id="ytplayer"
-				title="Youtube video"
-				src={`https://www.youtube.com/embed/${props.videoID}?autoplay=1&origin=${origin}&controls=0&disablekb=1&fs=0&loop=1&mute=1&playlist=${props.videoID}`}
-				allowFullScreen={true}
-				allow="autoplay"
-				style={{
-					width: "300%",
-					height: "100%",
-					marginLeft: "-100%",
-					border: "none"
-				}}></iframe>
+			<BrowserView>
+				<div style={{
+					overflow: "hidden",
+					aspectRatio: "19.5/9",
+					pointerEvents: "none",
+					borderRadius: "40px",
+					transform: "translateZ(0px)",
+					boxShadow: "-2px 5px 10px #000000"
+				}}>
+					<iframe 
+					id="ytplayer"
+					title="Youtube video"
+					src={`https://www.youtube.com/embed/${props.videoID}?autoplay=1&origin=${origin}&controls=0&disablekb=1&fs=0&loop=1&mute=1&playlist=${props.videoID}`}
+					allowFullScreen={true}
+					allow="autoplay"
+					style={{
+						width: "300%",
+						height: "100%",
+						marginLeft: "-100%",
+						border: "none"
+					}}></iframe>
 			
-			</div>
+				</div>
+			</BrowserView>
+			<MobileView>
+				<img src={`/media/${props.mobileThumbnail}`} alt="" style={{width: "100%", borderRadius: 15}} />
+			</MobileView>
 		</div>
 	)
 }
