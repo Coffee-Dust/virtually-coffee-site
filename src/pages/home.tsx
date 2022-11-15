2330e7a274f0136dcfb0409e82aa7e145b13ef5f
John Dust <d24dust@hotmail.com>
2022-11-15 17:27:48 -0500

    add: target attr to "_blank" on .virt8bitImage <a> tag parent

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,7 @@ const Home = () => (
       <hr />
     </div>
 
-    <a href="https://apps.apple.com/us/app/virtually-8-bit-game-console/id1634501013">
+    <a href="https://apps.apple.com/us/app/virtually-8-bit-game-console/id1634501013" target={"_blank"}>
     <img className={style.virt8bitImage} src="/images/downloadVirtually8bit.svg" alt="download" />
     </a>
 
